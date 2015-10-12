9b8634720d3463643533cc412014e4c3929fcaab
Arne Schauf <git@as-webservices.de>
2015-10-12 14:08:37 +0200

    add possibility to send color via mqtt

--- ax_textindicator.lua
+++ ax_textindicator.lua
@@ -11,12 +11,20 @@ local function start()
     m:on("message", function(conn, topic, data)
         print("received in "..topic.." => "..data)
         if data ~= nil then
-            if data == 't' then
-                ws2812.write(config.PIN_RGB_LED, string.char(255, 255, 255))
-                tmr.alarm(1, 50, 0, function ()
-                    ws2812.write(config.PIN_RGB_LED, string.char(0, 0, 0))
-                end)
+            local valid_json = false
+            if string.sub(data, 0, 1) == '{' then
+                local colors = cjson.decode(data)
+                if colors.r and colors.g and colors.b then
+                    ws2812.write(config.PIN_RGB_LED, string.char(colors.g, colors.r, colors.b))
+                    valid_json = true
+                end
             end
+            if not valid_json then
+                ws2812.write(config.PIN_RGB_LED, string.char(255, 255, 255))
+            end
+            tmr.alarm(1, 50, 0, function ()
+                ws2812.write(config.PIN_RGB_LED, string.char(0, 0, 0))
+            end)
         end
     end)
 
