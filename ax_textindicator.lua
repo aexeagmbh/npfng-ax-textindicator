b041e85e42114abbc15b63ef92e9630cbd2378d2
Arne Schauf <git@as-webservices.de>
2015-10-12 11:57:33 +0200

    indicate connect state

--- ax_textindicator.lua
+++ ax_textindicator.lua
@@ -1,6 +1,7 @@
 local config = require('config')
 
 local function start()
+    ws2812.write(config.PIN_RGB_LED, string.char(0, 255, 0))
     local m = mqtt.Client(node.chipid(), 120, "user", "password")
 
     m:on("connect", function(con) print ("connected") end)
@@ -20,7 +21,13 @@ local function start()
     end)
 
     m:connect(config.MQTT_HOST, config.MQTT_PORT, 0, function(conn)
-        m:subscribe("/text",0, function(conn) print("subscribe success") end)
+        m:subscribe("/text",0, function(conn) 
+            ws2812.write(config.PIN_RGB_LED, string.char(255, 0, 0))
+            tmr.alarm(1, 100, 0, function ()
+                ws2812.write(config.PIN_RGB_LED, string.char(0, 0, 0))
+            end)
+            print("subscribe success")
+        end)
     end)
 end
 
